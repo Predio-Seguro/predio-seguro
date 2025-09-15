c916f93d1699c9b3063e5305fc813359182fd03b
KaueB15 <kauecardozo3@gmail.com>
2025-09-14 23:06:08 -0300

    feat: add button to redirect for recovery password page

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -8,37 +8,37 @@ import { useNavigate } from "react-router-dom";
 const LoginForm = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm<LoginRequest>();
-    const [errorMessage, setErrorMessage] = useState("")
+    const [ errorMessage, setErrorMessage ] = useState("")
     const navigate = useNavigate()
     
 
     const onSubmit = async (data: LoginRequest) => {
 
         try {
+
             const response = await api.post("/auth/login/", data)
             if (response.status === 200) {
                 localStorage.setItem("token", response.data.token)
                 navigate("/home")
             }
 
-        } catch(error: any) {
+        } catch (error: any) {
 
             if (error.response.status === 401) {
                 setErrorMessage(error.response.data.detail)
-            }else if (error.response.status ==- 400) {
+            }else {
                 setErrorMessage("Erro do sistema tente novamente.")
             }
             
-        }
-        
+        } 
         
     }
     
     return (
 
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
-            {errorMessage && <span className="text-[15px] text-red-700 font-bold mb-6 uppercase">{errorMessage}</span>}
-            <div className="flex flex-col gap-12 mb-24">
+            {errorMessage && <span className="text-sm text-red-700 font-bold mb-6 uppercase">{errorMessage}</span>}
+            <div className="flex flex-col gap-12 mb-2.5">
                 <label className="flex flex-col gap-3">
                     <span className="text-2xl font-semibold">Nome de Usuário</span>
                     <input 
@@ -72,8 +72,9 @@ const LoginForm = () => {
                     {errors.password && <span className="text-red-700 font-bold uppercase text-sm">{errors.password.message}</span>}
                 </label>
             </div>
+            <a href="/email-recovery-password" className="mb-16 w-full  text-left font-bold transition duration-500 text-[#2D68C3] hover:text-[#1B3E75]">Esqueceu a senha?</a>
             <button className="w-96 h-16 bg-[#2D68C3] text-center rounded-2xl font-extrabold text-white 
-                                text-2xl cursor-pointer transition duration-500 hover:bg-[#1B3E75]" type="submit">Entrar</button>
+                            text-2xl cursor-pointer transition duration-500 hover:bg-[#1B3E75]" type="submit">Entrar</button>
         </form>
     )
 }
